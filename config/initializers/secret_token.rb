d63589cef6f52c2f3f3d22d9d628a1b343cc99e4
Paul Davis <paul@watermarktech.com>
2013-06-29 00:32:31 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyTag::Application.config.secret_token = '68dc5aaad3b8497ce8b00b86ed1ad779b526161352de1e1fda592995c76701e46060ef02d3e2f4294b16b4eec74cca879c5befa0e924a4c4acdbf8d6e11873ea'
